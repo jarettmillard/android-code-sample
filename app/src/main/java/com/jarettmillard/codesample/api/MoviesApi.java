67fa9f40ec15e4d3595db012b8b16ada07401648
Jarett Millard <jarett.millard@gmail.com>
2018-07-09 00:08:13 -0400

    Add movie details endpoint

--- app/src/main/java/com/jarettmillard/codesample/api/MoviesApi.java
+++ app/src/main/java/com/jarettmillard/codesample/api/MoviesApi.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface MoviesApi {
     @GET("top_movies.json")
     Call<List<Movie>> getTopMovies();
+
+    @GET("by_id/{imdbId}.json")
+    Call<Movie> getMovieDetails(@Path("imdbId") String imdbId);
 }
--- app/src/main/java/com/jarettmillard/codesample/api/models/Movie.java
+++ app/src/main/java/com/jarettmillard/codesample/api/models/Movie.java
@@ -1,14 +1,19 @@
 package com.jarettmillard.codesample.api.models;
 
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+
 import com.google.auto.value.AutoValue;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @AutoValue
-public abstract class Movie {
-    public abstract int rank();
+public abstract class Movie implements Parcelable {
+    @Nullable
+    public abstract Integer rank();
 
     public abstract String title();
 
@@ -22,7 +27,44 @@ public abstract class Movie {
 
     public abstract String poster();
 
+    @Nullable
     public abstract String imdbLink();
+
+    @Nullable
+    public abstract String rated();
+
+    @Nullable
+    public abstract String released();
+
+    @Nullable
+    public abstract String runtime();
+
+    @Nullable
+    public abstract List<String> genre();
+
+    @Nullable
+    public abstract String director();
+
+    @Nullable
+    public abstract String writer();
+
+    @Nullable
+    public abstract List<String> actors();
+
+    @Nullable
+    public abstract String plot();
+
+    @Nullable
+    public abstract List<String> language();
+
+    @Nullable
+    public abstract String country();
+
+    @Nullable
+    public abstract String awards();
+
+    @Nullable
+    public abstract String metascore();
 
     public static JsonAdapter<Movie> jsonAdapter(Moshi moshi) {
         return new com.jarettmillard.codesample.api.models.AutoValue_Movie.MoshiJsonAdapter(moshi);
